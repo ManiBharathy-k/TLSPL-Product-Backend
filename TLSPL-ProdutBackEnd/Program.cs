10d04971bb45acc7045dcfbea1c5fd8817256fd9
Mani <manibharthy363@gmail.com>
2025-06-18 23:56:03 +0530

    Commit002

--- TLSPL-ProdutBackEnd/Program.cs
+++ TLSPL-ProdutBackEnd/Program.cs
@@ -1,25 +1,41 @@
+using Microsoft.EntityFrameworkCore;
+using TLSPL_ProdutBackEnd.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Configure PostgreSQL
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+//  Allow all CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//  Use the "AllowAll" CORS policy
+app.UseCors("AllowAll");
 
-app.UseHttpsRedirection();
+app.UseSwagger();
+app.UseSwaggerUI();
+
+// Optional: disable if you're only using HTTP for local dev
+// app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
 
 app.Run();
+
